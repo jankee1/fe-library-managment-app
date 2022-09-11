2ad0f86542e51f6994f80166f716e771fdade2f0
Jan <jan.blaszczyk@protonmail.com>
2022-09-11 11:06:57 +0200

    Login and Register forms - basic functionality added (given data appears in console.log())

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,4 +1,27 @@
+import { stringify } from "querystring";
+import { useState } from "react";
+
 export const Login = () => {
+
+  interface LoginData {
+    email: string;
+    password: string
+  }
+
+  const [login, setLogin] = useState<LoginData>({
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const value = e.target.value;
+    setLogin({
+      ...login,
+      [e.target.name]: value
+    });
+  }
+
+
     return (
         <div className="login">
           <form action="">
@@ -6,11 +29,15 @@ export const Login = () => {
               type="text" 
               name="email" 
               placeholder="email"
+              required={true}
+              onChange={handleChange}
             />
             <input 
               type="password" 
               name="password" 
               placeholder="password"
+              required={true}
+              onChange={handleChange}
             />
             <button type="submit">Login</button>
           </form>
--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,33 +1,77 @@
+import { useState } from "react";
+import { Button } from "../common";
+
 export const Register = () => {
+
+  interface RegisterData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    repeatPassword: string;
+  }
+
+  const [register, setRegister] = useState<RegisterData>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const value = e.target.value;
+    setRegister({
+      ...register,
+      [e.target.name]: value
+    });
+  }
+
+  const handleRegisterForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(register)
+  }
+
     return (
-        <div className="register">
-          <form action="">
+        <div >
+          <form onSubmit={handleRegisterForm}>
             <input 
               type="text" 
               name="firstName" 
+              required={true}
               placeholder="First name"
+              onChange={handleChange}
             />
             <input 
               type="text" 
               name="lastName" 
+              required={true}
               placeholder="Last name"
+              onChange={handleChange}
             />
             <input 
-              type="text" 
+              type="email" 
               name="email" 
+              required={true}
               placeholder="Email"
+              onChange={handleChange}
             />
             <input 
               type="password" 
               name="password" 
+              required={true}
               placeholder="Password"
+              onChange={handleChange}
             />
             <input 
               type="password" 
               name="repeatPassword" 
+              required={true}
               placeholder="Confirm password"
+              onChange={handleChange}
             />
-            <button type="submit">Register</button>
+            <Button type="submit" text="Register"/>
           </form>
         </div>
   );
