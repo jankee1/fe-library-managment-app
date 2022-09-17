23106315b976e81cd48eb0ba187f3d12117cc22c
Jan <jan.blaszczyk@protonmail.com>
2022-09-17 17:57:01 +0200

    Books from library are loaded when user is logged in.

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -1,14 +1,17 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { usePrivateAxios } from "../../hooks/usePrivateAxios";
 import { AuthContext } from "../../context/AuthProvider";
 
 export const Logout = () => {
 
     const {setAuthUser} = useContext(AuthContext)
     const navigate = useNavigate();
+    const privateAxios = usePrivateAxios()
 
-    const handleLogout = () => {
-        setAuthUser(null)
+    const handleLogout = async () => {
+        await privateAxios.get('/auth/logout');
+        setAuthUser(() => null)
         navigate('/')
     }
 
--- src/components/library/UserLibrary.tsx
+++ src/components/library/UserLibrary.tsx
@@ -1,66 +1,59 @@
+import { useEffect, useState } from "react";
+import { usePrivateAxios } from "../../hooks/usePrivateAxios";
 import { SingleItemUserLibrary } from "./SingleItemUserLibrary";
+import {Book} from "types"
 
 
 export const UserLibrary = () => {
 
-    const books = [
-        {
-            title: "test",
-            author: "author test",
-            releaseDate: "2022",
-            numberOfAvailableBooks: 5
-        },
-        {
-            title: "test",
-            author: "author test",
-            releaseDate: "2022",
-            numberOfAvailableBooks: 5
-        },
-        {
-            title: "test",
-            author: "author test",
-            releaseDate: "2022",
-            numberOfAvailableBooks: 0
-        },
-        {
-            title: "test",
-            author: "author test",
-            releaseDate: "2022",
-            numberOfAvailableBooks: 5
-        },
-        {
-            title: "test",
-            author: "author test",
-            releaseDate: "2022",
-            numberOfAvailableBooks: 5
+    const privateAxios = usePrivateAxios()
+    const [booksInLibrary, setBooksInLibrary] = useState<Book [] | []>([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    const getBooks = async () => {
+        try {
+            const { data } = await privateAxios.get<Book []>('book')
+            setBooksInLibrary(data)
+            setIsLoaded(true);
+        } catch(e) {
+            console.error(e)
+            setIsLoaded(false)
         }
-    ]
+    }
+
+    useEffect( () => {
+        void getBooks();
+    }, [])
 
     return (
         <div>
-            <table>
-            <thead>
-            <tr>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Date of release</th>
-                <th>In stock</th>
-                <th>Action</th>
-            </tr>
-            </thead>
-                <tbody>
-                    {
-                        books.map(
-                            book => <SingleItemUserLibrary 
-                                key={Math.floor(Math.random() * (1000000 - 1 + 1)) + 1} 
-                                title={book.title} 
-                                author={book.author} 
-                                releaseDate={book.releaseDate} 
-                                numberOfAvailableBooks={book.numberOfAvailableBooks} 
-                            />)
-                    }
-                </tbody>
-            </table>
+            {!isLoaded && <p>loading...</p>}
+            {isLoaded && 
+                <table>
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Date of release</th>
+                    <th>In stock</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                    <tbody>
+                        {
+                            booksInLibrary.map(
+                                (book: Book) => <SingleItemUserLibrary 
+                                    key={Math.floor(Math.random() * (1000000 - 1 + 1)) + 1} 
+                                    title={book.title} 
+                                    author={`${book.authorFirstName} ${book.authorLastName}`} 
+                                    releaseDate={new Date(book.publishedOn).toDateString()} 
+                                    numberOfAvailableBooks={book.numberOfAvailable} 
+                                />)
+                        }
+                    </tbody>
+                </table>
+            }
+            
         </div>
   );
 }
