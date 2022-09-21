ee04d0ff5937fce080fb37f2072e86e2921624ad
Jan <jan.blaszczyk@protonmail.com>
2022-09-21 20:55:33 +0200

    Update of book details and real time changes fully operational.

--- src/components/library/SingleItemAdminLibrary.tsx
+++ src/components/library/SingleItemAdminLibrary.tsx
@@ -12,7 +12,7 @@ interface SingleItemLibraryProps {
     title:string;
     releaseDate: string;
     numberOfAvailableBooks: number;
-    setIsLoaded: React.Dispatch<React.SetStateAction<boolean>>
+    setIsLoaded: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const SingleItemAdminLibrary = (props: SingleItemLibraryProps) => {
@@ -26,15 +26,15 @@ export const SingleItemAdminLibrary = (props: SingleItemLibraryProps) => {
         publishedOn: new Date(props.releaseDate),
         title: ''
     })
+    const [isLoaded, setIsLoaded] = useState(false);
 
     const updateBookDetails = async (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>): Promise<void> => {
         e.preventDefault();
         const value = e.target.value;
         setBookDetails({
             ...bookDetails,
-            [e.target.name]: value
+            [e.target.name]: e.target.name !== "numberOfAvailable" ? value : Number(value)
           });
-          console.log(bookDetails)
     }
 
     const showEditForm = () => {
@@ -53,6 +53,8 @@ export const SingleItemAdminLibrary = (props: SingleItemLibraryProps) => {
         try{ 
             const date =  await privateAxios.patch(`book/${props.id}`, bookDetails)
             props.setIsLoaded(false)
+            setIsLoaded(false)
+            console.log(bookDetails)
         }catch(e) {console.error(e)}
     }
 
@@ -61,49 +63,63 @@ export const SingleItemAdminLibrary = (props: SingleItemLibraryProps) => {
                 authorFirstName: props.authorFirstName,
                 authorLastName: props.authorLastName,
                 numberOfAvailable: props.numberOfAvailableBooks,
-                publishedOn: new Date(bookDetails.publishedOn) ,
+                publishedOn: new Date(props.releaseDate) ,
                 title: props.title
-              });
-              
-    },[])
+            });
+            setIsLoaded(true)
+    },[isLoaded])
 
     return (
             <>
-            <tr>
-                <td>{props.title}</td>
-                <td>{`${props.authorFirstName} ${props.authorLastName}`}</td>
-                <td>{props.releaseDate}</td>
-                <td>{props.numberOfAvailableBooks}</td>
-                <td>
-                    <button type="button" onClick={() => showEditForm()}>Edit</button>
-                    <button type="button" onClick={() => handleDelete(props.id)}>Delete</button>
-                </td>
-            </tr>
-            { editForm  && 
+            {!isLoaded && 
+                <tr>
+                    <td colSpan={5}>
+                        <p>loading...</p>
+                    </td>
+                </tr>
+            }
+
+            {isLoaded && 
+                <tr>
+                    <td>{props.title}</td>
+                    <td>{`${props.authorFirstName} ${props.authorLastName}`}</td>
+                    <td>{props.releaseDate}</td>
+                    <td>{props.numberOfAvailableBooks}</td>
+                    <td>
+                        <button type="button" onClick={() => showEditForm()}>Edit</button>
+                        <button type="button" onClick={() => handleDelete(props.id)}>Delete</button>
+                    </td>
+                </tr>
+            }
+
+            { editForm && isLoaded &&
         
-            <tr >
-                <td colSpan={5} >
-                    <form onSubmit={handleEditForm}>
-                        <p>Title</p>
-                        <input type="text" name="title" id="" value={bookDetails.title} onChange={updateBookDetails} />
-                        <p>Author's first name</p>
-                        <input type="text" name="authorFirstName" id="" value={bookDetails.authorFirstName} onChange={updateBookDetails} />
-                        <p>Author's last name</p>
-                        <input type="text" name="authorLastName" id="" value={bookDetails.authorLastName} onChange={updateBookDetails} />
-                        <p>Release date</p>
-                        <DatePicker selected={bookDetails.publishedOn} onChange={(date) =>  {
-                            if(date === null)
-                            date = new Date()
-                            setBookDetails({...bookDetails, publishedOn: date})
-                        }} />
-                        <p>In Stock</p>
-                        <input type="number" name="numberOfAvailable" id="" value={bookDetails.numberOfAvailable} onChange={updateBookDetails} />
-                        <button type="submit">Update book</button> 
-                    </form>
-                </td>
-            </tr>
+                <tr >
+                    <td colSpan={5} >
+                        <form onSubmit={handleEditForm}>
+                            <p>Title</p>
+                            <input type="text" name="title" id="" value={bookDetails.title} onChange={updateBookDetails} />
+                            <p>Author's first name</p>
+                            <input type="text" name="authorFirstName" id="" value={bookDetails.authorFirstName} onChange={updateBookDetails} />
+                            <p>Author's last name</p>
+                            <input type="text" name="authorLastName" id="" value={bookDetails.authorLastName} onChange={updateBookDetails} />
+                            <p>Release date</p>
+                            <DatePicker selected={bookDetails.publishedOn} onChange={(date) =>  {
+                                    if(date === null)
+                                        date = new Date()
+                                    setBookDetails({...bookDetails, publishedOn: date})
+                                }} 
+                                dateFormat="dd/MM/yyyy"
+                            />
+                            <p>In Stock</p>
+                            <input type="number" name="numberOfAvailable" id="" min={1} value={bookDetails.numberOfAvailable} onChange={updateBookDetails} />
+                            <button type="submit">Update book</button> 
+                        </form>
+                    </td>
+                </tr>
             
             }
+            
             </>
             
   );
