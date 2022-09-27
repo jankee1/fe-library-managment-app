d6d0b4d1dc4198e565889696fcaf4d1539240e4c
Jan <jan.blaszczyk@protonmail.com>
2022-09-27 17:10:02 +0200

    Error message added when page is not found

--- src/components/page-not-found/PageNotFound.tsx
+++ src/components/page-not-found/PageNotFound.tsx
@@ -1,7 +1,12 @@
+import './PageNotFound.css'
+
 export const PageNotFound = () => {
     return (
-        <div>
-            <p>Page has not been found!</p>
+        <div className="page-not-found">
+            <div>
+                <h3>404</h3>
+                <p>Page has not been found!</p>
+            </div>
         </div>
   );
 }
